1fddb61eb1c8db6ed7dd49640b2f124003680754
Rahat Maksud Pranto <49338951+prantorahat007@users.noreply.github.com>
2021-06-22 01:38:12 +0600

    making  form and connect database basic

---
+++ Lab5/Registration.php
@@ -0,0 +1,63 @@
+<html>
+   <head> 
+      
+      
+   </head>
+   
+    <body>
+      <fieldset style ="width:750px">
+          <legend><h1> Registration</h1></legend>
+      
+             <form action="" method="post">
+      
+                <table>
+        <tr>
+            <th>No.</th>
+            <th>Registered</th>
+            <th>year</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bangla</td>
+            <td>1st paper</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>English</td>
+            <td>1st paper</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Higher Math</td>
+            <td>1st paper</td>
+        </tr>
+		<tr>
+            <td>4</td>
+            <td>chemistry</td>
+            <td>1st paper</td>
+        </tr>
+		<tr>
+            <td>5</td>
+            <td>physics</td>
+            <td>1st paper</td>
+        </tr>
+		<tr>
+            <td>6</td>
+            <td>Ict</td>
+            <td>1st part</td>
+        </tr>
+		<tr>
+            <td>7</td>
+            <td>Biology</td>
+            <td>1st paper</td>
+        </tr>
+    </table>
+			 
+			  <button><a href="dashboard.php">back</a></button>
+
+		<button onclick="window.print()">Print this page</button>
+      </body>
+
+ 
+
+</html>
